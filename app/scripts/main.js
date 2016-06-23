d1dd97f8b0ca977aff0c3eeeed2ba36be19cf06a
Cyclonic <cylconicalex@gmail.com>
2016-06-23 17:13:51 +0200

    modification du fichier ./scripts/main.js pour prendre en charge les graphique chart.js

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -4,3 +4,40 @@ $(document).ready(function(){
   	speed: 1000
   });
 });
+
+$(".progress").each(function(){
+	var pourcentConnu = $(this).find(".progress-bar").attr("aria-valuenow");
+	var pourcentRestant = $(this).find(".progress-bar").attr("aria-valuemax") - pourcentConnu;
+	var couleur = $(this).find(".progress-bar").css('background-color');
+	var couleurFonds = $(this).parent().css('background-color');
+	var newElem = $('<canvas></canvas>');
+	$(this).replaceWith(newElem);
+
+
+	var data = {
+    labels: [],
+    datasets: [
+        {
+            data: [pourcentConnu, pourcentRestant],
+            backgroundColor: [
+                couleur,
+                couleurFonds
+            ],
+            hoverBackgroundColor: [
+                couleur,
+                couleurFonds
+            ]
+        }]
+	};
+
+	var myChart = new Chart(newElem,{
+	    type:"doughnut",
+	    data: data,
+	    animation:{
+	        animateScale:true
+	    },
+	    options:{
+	    	tooltips: false
+	    }
+	});
+});
